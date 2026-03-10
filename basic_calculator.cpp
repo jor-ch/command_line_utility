af1371ecbc3e8f657bfa0950d0f5521df4c3841d
jor-ch <chtoh97@gmail.com>
2026-03-10 11:21:29 +0800

    basic interface for basic_calculator, before processing the input strings and doing the actual calculations
    basic interface for basic_calculator, before processing the input strings and doing the actual calculations

---
+++ basic_calculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <limits>
+#include "includes/utilities.h"
+#include <optional>
+#include <vector>
+#include <unordered_map>
+void basicCalculatorInterface()
+{
+    std::cout << "Welcome to Basic Calculator! Here, we will perform basic arithmetic operations!" << std::endl;
+    std::cout << "type in the statement. This calculator is only limited to +, -, * and / operations." << std::endl;
+    std::cout << "Example: 2 + 3 * 4 - 5 / 6" << std::endl;
+    std::cout << "Please leave a space between the numbers and operators!" << std::endl;
+    std::string input;
+    std::optional<double> result = std::nullopt;
+}
