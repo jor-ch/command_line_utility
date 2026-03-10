c7b4e63c08c86d393a7c9cc0a3497d39f1297531
jor-ch <chtoh97@gmail.com>
2026-03-10 12:26:06 +0800

    removed the test class. Added constants for celsius to farenheit conversion, needed for temperature_converter
    removed the test class. Added constants for celsius to farenheit conversion, needed for temperature_converter

--- includes/utilities.h
+++ includes/utilities.h
@@ -1,15 +1,7 @@
 #pragma once
 #include <iostream>
 
-class test
-{
-public:
-    test() : var1(3.0), var2(5) {}
-    void printVar() { std::cout << "vars are " << var1 << " " << var2 << std::endl; }
-
-private:
-    double var1;
-    int var2;
-};
+constexpr double CELSIUS_TO_FAHRENHEIT_MULTIPLIER = 9.0 / 5.0;
+constexpr double CELSIUS_TO_FAHRENHEIT_CONSTANT = 32.0;
 
 bool checkContinue();
