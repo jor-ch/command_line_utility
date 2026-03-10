b19bc56c756b77dfbcc12421a439067207dd60a9
jor-ch <chtoh97@gmail.com>
2026-03-10 20:38:58 +0800

    shifted printout of inputting temperature and letter to inside while loop, so that the instructions would keep printing when user wants to continue
    shifted printout of inputting temperature and letter to inside while loop, so that the instructions would keep printing when user wants to continue

--- temperature_converter.cpp
+++ temperature_converter.cpp
@@ -42,11 +42,11 @@ void temperatureConverterInterface()
     std::cout << "Note: currently, we only support conversion between Celsius and Fahrenheit!" << std::endl;
     std::cout << "Format of input is temperature value and C/F, to indicate if temperature is in Celsius or Farenheit" << std::endl;
     std::cout << "Example: 32.5 C  (this is to convert 32.5 degrees celsius to farenheit)" << std::endl;
-    std::cout << "Please enter the temperature value, followed by a space then letter:" << std::endl;
     std::string input;
     std::optional<double> result = std::nullopt;
     while (true)
     {
+        std::cout << "Please enter the temperature value, followed by a space then letter:" << std::endl;
         std::getline(std::cin, input);
         result = performConversion(input);
         if (result.has_value())
