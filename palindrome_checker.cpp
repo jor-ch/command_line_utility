2a369048bca6ae98af71c81f796f5e928c363d0a
jor-ch <chtoh97@gmail.com>
2026-03-28 23:08:20 +0800

    rectified an error in palindrome_checker in that it takes two strings instead of one.
    rectified an error in palindrome_checker in that it takes two strings instead of one. Initially, two strings were taken, and how the palindrome check was carried out is that it checks if both strings are palindromes of each other, which does not make sense, because the user has to input the same string twice. This is now rectified, and the interface only takes one string input from the user to get the string to be checked.

--- palindrome_checker.cpp
+++ palindrome_checker.cpp
@@ -6,17 +6,14 @@
 #include <algorithm>
 #include <limits>
 #include "includes/utilities.h"
-bool checkPalindrome(const std::string &str1, const std::string &str2)
+bool checkPalindrome(const std::string &str)
 {
-    int str1Len = str1.length();
-    int str2Len = str2.length();
-    if (str1Len != str2Len)
-        return false; // since the lengths are different, they cannot be palindromes of each other
-    // here, we use two pointers to compare the characters of the strings, ignoring case
-    int left_idx = 0, right_idx = str2Len - 1;
-    while (left_idx < str1Len && right_idx >= 0)
+    int strLen = str.length();
+    // here, we use two pointers to compare the characters of the string, ignoring case
+    int left_idx = 0, right_idx = strLen - 1;
+    while (left_idx < right_idx)
     {
-        if (std::tolower(str1[left_idx]) != std::tolower(str2[right_idx]))
+        if (std::tolower(str[left_idx]) != std::tolower(str[right_idx]))
             return false;
         left_idx++;
         right_idx--;
@@ -26,23 +23,19 @@ bool checkPalindrome(const std::string &str1, const std::string &str2)
 
 void palindromeCheckerInterface()
 {
-    std::cout << "Welcome to Palindrome checker! Here, we will check if two strings are Palindrome!" << std::endl;
+    std::cout << "Welcome to Palindrome checker! Here, we will check if a string is a Palindrome!" << std::endl;
     std::cout << "This checker is case-insensitive and you can include special characters!" << std::endl;
-    std::string string1, string2;
+    std::string inputString;
     bool isPalindrome;
     while (true)
     {
-        string1.clear();
-        string2.clear();
+        inputString.clear();
         isPalindrome = false;
-        std::cout << "enter first string" << std::endl;
-        std::getline(std::cin, string1);
-        std::cout << "string 1 is " << string1 << std::endl;
-        std::cout << "enter second string" << std::endl;
-        std::getline(std::cin, string2);
-        std::cout << "string 2 is " << string2 << std::endl;
-        isPalindrome = checkPalindrome(string1, string2);
-        std::cout << "Both strings given are " << (isPalindrome ? "" : "NOT ") << "palindromes!" << std::endl;
+        std::cout << "enter a string" << std::endl;
+        std::getline(std::cin, inputString);
+        std::cout << "string is " << inputString << std::endl;
+        isPalindrome = checkPalindrome(inputString);
+        std::cout << "The string is " << (isPalindrome ? "" : "NOT ") << "a palindrome!" << std::endl;
         if (!checkContinue())
             return;
     }
